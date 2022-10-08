33c6edd045ba17f9dbc5142597b1b28a560dc512
Ethan Sabini <ConwaySabini@gmail.com>
2022-10-07 21:48:23 -0700

    getty

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -95,17 +95,17 @@ function Card({ article, type }) {
     if (type === "trending") {
         return (
             <div class="card" id="news-card">
-                <div class="card-content" id="news-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-4by3">
-                                <img src={thumbnail} alt="profile" />
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="title is-4" id="card-title">{name}</p>
-                        </div>
+                <div class="media">
+                    <div class="media-left">
+                        <figure class="image is-4by3">
+                            <img src={thumbnail} alt="profile" />
+                        </figure>
                     </div>
+                    <div class="media-content">
+                        <p class="title is-4" id="card-title">{name}</p>
+                    </div>
+                </div>
+                <div class="card-content" id="news-content">
                     <div class="content">
                         {organization}<br /><br />
                         {description}
